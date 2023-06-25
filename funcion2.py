b50ec3dea4f170aa5b1b5edcc3c54d2e5a130ad2
Eg139 <eric.guzman.139@gmail.com>
2023-06-25 19:58:44 -0300

    correcciones practica

--- funcion2.py
+++ funcion2.py
@@ -3,7 +3,7 @@
 #  segundo parámetro por el tercero y devolver la cantidad de veces que se reemplazo ese carácter  en la
 #  cadena
 
-def reemplazarCaracteres(cadena:str,caracter_uno:str,caracter_dos:str)->str:
+def reemplazarCaracteres(cadena:str,caracter_uno:str,caracter_dos:str)->int:
     """reemplaza caracteres recibidos de una cadena, buscando el primer parametro para reemplazarlo por el segundo
 
     Args:
@@ -12,14 +12,16 @@ def reemplazarCaracteres(cadena:str,caracter_uno:str,caracter_dos:str)->str:
         caracter_dos (str): parametro a cambiar
 
     Returns:
-        str: retorna la cadena con los cambios
+        int: retorna la cantidad de veces que se reemplazo ese carácter  en la cadena
     """
     aux = ""
+    contador = 0
     for letra in cadena:
         if letra == caracter_uno:
             aux += caracter_dos
+            contador += 1
         else:
             aux += letra
-    return aux
+    return contador
 
-print(reemplazarCaracteres("pepito","t","z"))
+print("La cantidad de reemplazos fue de: ",reemplazarCaracteres("pepito","t","z"))
--- funcion3.py
+++ funcion3.py
@@ -2,8 +2,40 @@
 #  su responsabilidad es ordenar los caracteres de manera ascendente dentro de la cadena. 
 # Ejemplo si le pasamos "algoritmo" la deja como "agilmoort"
 
-def ordenar_caracteres(cadena)->str:
+def ordenar_caracteres(cadena:str)->str:
+    """ordenar los caracteres de manera ascendente dentro de la cadena.
 
-    return 
+    Args:
+        cadena (str): Cadena que debe ordenarze ascendentemente
 
-print(ordenar_caracteres("pepito"))
+    Returns:
+        str: cadena ordenada
+    """
+    aux_cadena = list(cadena)
+    retorno = ""
+
+    for i in range(len(aux_cadena)):
+        for j in range(i+1, len(aux_cadena)):
+            if aux_cadena[i] > aux_cadena[j]:
+                aux = aux_cadena[j]
+                aux_cadena[j] = aux_cadena[i]
+                aux_cadena[i] = aux
+
+    for letra in aux_cadena:
+        retorno += letra
+
+    return retorno
+
+def ordenar_numeros(cadena:list)->list:
+
+    for i in range(len(cadena)):
+        for j in range(i+1, len(cadena)):
+            if cadena[i] > cadena[j]:
+                aux = cadena[j]
+                cadena[j] = cadena[i]
+                cadena[i] = aux
+
+    return cadena
+
+print(ordenar_caracteres("algoritmo"))
+print(ordenar_numeros([0,8,45,1,320,21,51,10,2]))
